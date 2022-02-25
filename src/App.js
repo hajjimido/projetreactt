b6413f21eca509749d7abf2ab154e4b8b77bcfef
Mehdi hajji <mhajji805@gmail.com>
2022-02-25 12:15:55 +0100

    etudiant

--- src/App.js
+++ src/App.js
@@ -2,45 +2,51 @@
 import './App.css';
 import {BrowserRouter as Router , Route, Switch} from 'react-router-dom';
 import { Link } from 'react-router-dom';
-
-
-
-import Modifier from "./pages/modifier";
-import Ajouter from "./pages/ajouter";
+import "./App.css"
 import { Nav } from 'react-bootstrap';
+
+import Home from "./pages/home";
+
+
 
 
  
   function App(){
     return(
    <Router>
-     <div>
-          <Nav fill variant="tabs" defaultActiveKey="/home">
-  <Nav.Item>
-    <Nav.Link as={Link} to={"/ajouter"} >ajouter un plat</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link as={Link} to={"/modifier"} >modifier un plat</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-2">nos plats</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="disabled" disabled>
-      Disabled
-    </Nav.Link>
-  </Nav.Item>
-</Nav>
+   <div>
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Resto</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <Nav.Link as={Link} to={"/home"}>Home </Nav.Link>
+      </li>
+      <li class="nav-item">
+        <Nav.Link as={Link} to={"/About"} type="button">About</Nav.Link>
+      </li>
+      <li class="nav-item">
+        <Nav.Link as={Link} to={"/home"}>Menu</Nav.Link>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
           </div>
 
       <div>
       <Switch>
-      <Route path="/ajouter" component={Ajouter}/>
-       <Route path="/modifier" component={Modifier}/>
+      <Route path="/home" component={Home}/>
+      <Route path="/home" component={Home}/>
+       
        </Switch>
        </div>
       
    </Router>
+
  
       );
     }
---
+++ src/pages/home.js
@@ -0,0 +1,66 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+// import required modules
+import { EffectFade, Navigation, Pagination } from "swiper";
+import { Component } from "react";
+import logo from '../logo.png'
+import chef from '../chef.png'
+import bg from '../bg.jpg'
+
+class home extends Component{
+    render(){
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        effect={"fade"}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[EffectFade, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+        <div class="container">
+                         
+        <div class="row gx-3">
+        <div class="row gy-5">
+        <div class="col-lg-5">
+        <img src={logo} class="img-fluid" alt=""/>
+         </div>
+        <div class="col-lg-7">
+        <h1 >Our Special Dish</h1>
+            <h1>Spicy Noodles</h1>
+            <button>Order Now</button>
+            </div>
+            </div>
+            </div>
+            </div>
+              
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+}
+export default home;
